b3300d35a9a16bc42773fed1a581f510d6d7dfac
OH-front-end <oh.frontend@gmail.com>
2024-05-11 16:57:45 +0300

    support page modified

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,5 +1,18 @@
+import styles from './Support.module.css';
+import supportImg from '../../assets/support.png';
+
 const Support = () => {
-  return <h1>Служба підтримки</h1>;
+  return (
+    <div className={styles.support_container}>
+      <div className={styles.support_header}>
+        <img className={styles.support_img} src={supportImg} alt="" />
+        <div className={styles.support_title}>
+          <span style={{ fontSize: '70px', letterSpacing: '4px' }}>Служба</span> підтримки
+          <span className={styles.support_caption}>клієнтів</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Support;
